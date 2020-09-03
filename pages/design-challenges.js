f2fdf1f74dfadde03534dc9dab52752a3e5a5bc7
Brendan Norris <brendan.t.norris@gmail.com>
2020-09-03 12:27:54 -0700

    added 2nd featured photo to design challenges

--- pages/design-challenges.js
+++ pages/design-challenges.js
@@ -4,6 +4,10 @@ import ProjectLayout from "../layouts/ProjectLayout";
 const DesignChallenges = () => {
   const images = [
     {
+      caption: "The challenge: The perfect lobby.",
+      image: "projects/designchallenges/challenges-7.png",
+    },
+    {
       caption: "The challenge: A space using only Black-made/owned pieces.",
       image: "projects/designchallenges/challenges-5.png",
     },
@@ -15,10 +19,6 @@ const DesignChallenges = () => {
       caption:
         "The challenge: A reading nook in your dream vacation home in Mexico.",
       image: "projects/designchallenges/challenges-3.png",
-    },
-    {
-      caption: "The challenge: The perfect lobby.",
-      image: "projects/designchallenges/challenges-7.png",
     },
     {
       caption: "The challenge: Upping your bookshelf game.",
@@ -36,8 +36,10 @@ const DesignChallenges = () => {
       imageURL="/projects/designchallenges/cover.jpg"
       caption="The challenge: Design the perfect Zoom background."
     >
-      <section className="section py-2">
+      <section className="section text-center md:pt-10">
         <div className="container">
+          <img src={images[0].image} className="rounded-lg" />
+          <p className="pt-4">{images[0].caption}</p>
           {/* <div className="project-description py-10 text-center">
             This is a collection of work I've done for clients at Spoak.
           </div> */}
@@ -50,7 +52,7 @@ const DesignChallenges = () => {
             className="my-masonry-grid -mx-4"
             columnClassName="my-masonry-grid_column mx-4"
           >
-            {images.map((item, index) => (
+            {images.slice(1).map((item, index) => (
               <div className="grid-item" key={index}>
                 <img src={item.image} className="rounded-lg" />
                 {item.caption && <p className="pt-4">{item.caption}</p>}
